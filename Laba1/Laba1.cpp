22401de7b934cda9d8e9ba870e7e3707d1e8c0d7
KraM93 <112386261+KraM93@users.noreply.github.com>
2023-06-11 13:42:38 +0300

    first commit

---
+++ Laba1/Laba1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+class Rectangle
+{
+private:
+    double length;
+    double width;
+public:
+    Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+    double getPerimeter()
+    {
+        return 2 * (length + width);
+    }
+    double getArea()
+    {
+        return length * width;
+    }
+    double getDiagonalLength()
+    {
+        return sqrt((length * length) + (width * width));
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    {
+        Rectangle rectangle(5.0, 3.0);
+        cout << "Периметр: " << rectangle.getPerimeter() << endl;
+        cout << "Площадь: " << rectangle.getArea() << endl;
+        cout << "Длина диагонали: " << rectangle.getDiagonalLength() << endl;
+     
+               return 0;
+    }
+}
+
+
+
+
+
